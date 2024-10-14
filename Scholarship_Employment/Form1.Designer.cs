fd48bfe253aae00763a21362cffda26cd2fe3e1e
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2024-10-14 16:09:45 +0800

    add project to repo

---
+++ Scholarship_Employment/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Scholarship_Employment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMiddleInitial = new System.Windows.Forms.TextBox();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastname.Location = new System.Drawing.Point(54, 70);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(300, 26);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSubmit.Location = new System.Drawing.Point(54, 360);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(132, 48);
+            this.btnSubmit.TabIndex = 14;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last name: ";
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstname.Location = new System.Drawing.Point(54, 150);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(300, 26);
+            this.txtFirstname.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Middle initial: ";
+            // 
+            // txtMiddleInitial
+            // 
+            this.txtMiddleInitial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleInitial.Location = new System.Drawing.Point(54, 230);
+            this.txtMiddleInitial.Name = "txtMiddleInitial";
+            this.txtMiddleInitial.Size = new System.Drawing.Size(150, 26);
+            this.txtMiddleInitial.TabIndex = 3;
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSuffix.Location = new System.Drawing.Point(54, 310);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(150, 26);
+            this.txtSuffix.TabIndex = 4;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(50, 280);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(65, 20);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Suffix: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 445);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMiddleInitial);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtLastname);
+            this.Name = "Form1";
+            this.Text = "Scholarship Employment";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMiddleInitial;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Scholarship_Employment/Form1.cs
@@ -0,0 +1,75 @@
+﻿using MySqlConnector;
+using System;
+using System.Windows.Forms;
+
+namespace Scholarship_Employment
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnectionStringBuilder _conn = null;
+        MySqlCommand _command = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _conn = new MySqlConnectionStringBuilder();
+            _conn.Server = "localhost";
+            _conn.UserID = "root";
+            _conn.Password = "Mysql.Tesda2024";
+            _conn.Database = "tesda_db";
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string last_name = txtLastname.Text;
+            string first_name = txtFirstname.Text;
+            string middle_initial = txtMiddleInitial.Text;
+            string suffix = txtSuffix.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(_conn.ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string sql = "CALL check_fullname(@last, @first)";
+                    _command = new MySqlCommand(sql, connection);
+                    _command.Parameters.AddWithValue("@last", last_name.ToUpper());
+                    _command.Parameters.AddWithValue("@first", first_name.ToUpper());
+                    _command.ExecuteNonQuery();
+
+                    string readLastname = "";
+                    string readFirstname = "";
+                    MySqlDataReader reader = _command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        readLastname += $"{reader.GetString(0)}";
+                        readFirstname += $"{reader.GetString(1)}";
+                    }
+
+                    if (readLastname.Equals("") && readFirstname.Equals(""))
+                    {
+                        Form2 form = new Form2();
+                        form.SetFullname(last_name, first_name, middle_initial, suffix);
+                        form.Show();
+                        Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("This name already exists.", "Existing Name");
+                    }
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
