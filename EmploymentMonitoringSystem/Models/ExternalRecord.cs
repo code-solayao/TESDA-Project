5f270aceaed917032628dec5b2e31a8034e4c71c
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2024-12-23 16:15:26 +0800

    Changed data types to non-nullable
    - Changed all string data types to non-nullable and set default values to string.Empty
    - Planned to change their nullable states to "true" in the migration files

--- EmploymentMonitoringSystem/Models/ExternalRecord.cs
+++ EmploymentMonitoringSystem/Models/ExternalRecord.cs
@@ -4,18 +4,18 @@
     {
         public int Id { get; set; }
 
-        public string? training_status { get; set; }
-        public string? assessment_result { get; set; }
-        public string? employment_before_training { get; set; }
-        public string? occupation { get; set; }
-        public string? employer_name { get; set; }
-        public string? employment_type { get; set; }
-        public string? date_hired { get; set; }
-        public string? remarks { get; set; }
-        public string? count { get; set; }
-        public string? no_of_graduates { get; set; }
-        public string? no_of_employed { get; set; }
-        public string? verification { get; set; }
-        public string? job_vacancies { get; set; }
+        public string training_status { get; set; } = string.Empty;
+        public string assessment_result { get; set; } = string.Empty;
+        public string employment_before_training { get; set; } = string.Empty;
+        public string occupation { get; set; } = string.Empty;
+        public string employer_name { get; set; } = string.Empty;
+        public string employment_type { get; set; } = string.Empty;
+        public string date_hired { get; set; } = string.Empty;
+        public string remarks { get; set; } = string.Empty;
+        public string count { get; set; } = string.Empty;
+        public string no_of_graduates { get; set; } = string.Empty;
+        public string no_of_employed { get; set; } = string.Empty;
+        public string verification { get; set; } = string.Empty;
+        public string job_vacancies { get; set; } = string.Empty;
     }
 }
